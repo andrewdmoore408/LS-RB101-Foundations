d105e83f7131637edb2a6a14deb7aa95205cc65d
Andrew Moore <andrewdmoore84@gmail.com>
2021-01-31 01:28:37 -0800

    Add match functionality: first player to win 5 takes the match

--- lesson_2/lizard_spock.rb
+++ lesson_2/lizard_spock.rb
@@ -20,13 +20,21 @@ def win?(first, second)
   DEFEATS[first].include?(second)
 end
 
-def display_result(choice, computer_choice)
+def get_result(choice, computer_choice)
   if win?(choice, computer_choice)
-    prompt("You won!")
+    :player 
   elsif win?(computer_choice, choice)
-    prompt("Computer won!")
+    :computer 
   else
-    prompt("You tied!")
+    :tie
+  end
+end
+
+def display_result(winner)
+  case winner
+  when :player then prompt "You won!"
+  when :computer then prompt "Computer won!"
+  when :tie then prompt "Tie...no winner..."
   end
 end
 
@@ -34,35 +42,68 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def display_points(points)
+  scores = <<-SCORE
+  You: #{points[:player]}     -     Computer: #{points[:computer]}
+            Ties: #{points[:tie]}
+  SCORE
+
+  prompt("\n\n#{scores}\n\n")
+end
+
+# main game loop
 loop do
-  choice = ''
 
+  points = { player: 0, computer: 0, tie: 0 }
+  keep_playing = 'n'
+  
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    prompt("You can also just use first letter (k for spock)")
-    choice = Kernel.gets().chomp()
-
-    if choice.size == 1 && CHOICE_ABBREVIATIONS[choice]
-      choice = CHOICE_ABBREVIATIONS[choice]
+    choice = ''
+  
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      prompt("You can also just use first letter (k for spock)")
+      choice = Kernel.gets().chomp()
+  
+      if choice.size == 1 && CHOICE_ABBREVIATIONS[choice]
+        choice = CHOICE_ABBREVIATIONS[choice]
+      end
+  
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+  
+    computer_choice = VALID_CHOICES.sample
+  
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+  
+    result = get_result(choice, computer_choice)
+    display_result(result)
 
-    if VALID_CHOICES.include?(choice)
+    points[result] += 1
+
+    if points[:player] == 5 || points[:computer] == 5
+      prompt("\n\nGAME OVER!\n\n#{points[:player] == 5 ? "You" : "Computer"} won!")
       break
     else
-      prompt("That's not a valid choice.")
+      display_points(points)
     end
-  end
+    
+    prompt("Do you want to keep going?")
+    keep_playing = Kernel.gets().chomp()
+  
+    break unless keep_playing.downcase().start_with?("y")
+  end 
 
-  computer_choice = VALID_CHOICES.sample
+  break if keep_playing != 'y'
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+  prompt("Good game! Another round?")
 
-  display_result(choice, computer_choice)
-
-  prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
-
-  break unless answer.downcase().start_with?("y")
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt "Thank you for playing. Goodbye!"
